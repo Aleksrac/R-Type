d226653db0bc862b9f21ad7707c9607754eff248
Aleksandra <aleksandra.racine@epitech.eu>
2025-11-28 21:03:24 +0100

    refactor(ecs): refactor file (header epitech, put code in cpp)

--- GameEngine/Systems/InputSystem.cpp
+++ GameEngine/Systems/InputSystem.cpp
@@ -1,5 +1,20 @@
-//
-// Created by aleks on 27/11/2025.
-//
+/*
+** EPITECH PROJECT, 2025
+** R_Type
+** File description:
+** InputSystem
+*/
 
 #include "InputSystem.hpp"
+
+void InputSystem::update(EcsManager & ecs) {
+    for (auto& entity : ecs.getEntities()) {
+        auto inputPtr = entity->getComponent<InputPlayer>();
+        if (!inputPtr) continue;
+
+        inputPtr->_up = sf::Keyboard::isKeyPressed(sf::Keyboard::W);
+        inputPtr->_down = sf::Keyboard::isKeyPressed(sf::Keyboard::S);
+        inputPtr->_left = sf::Keyboard::isKeyPressed(sf::Keyboard::A);
+        inputPtr->_right = sf::Keyboard::isKeyPressed(sf::Keyboard::D);
+    }
+}
