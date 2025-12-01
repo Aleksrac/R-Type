e781d494c47c08a35530e4d2d934f8df9d18efff
Aleksandra <aleksandra.racine@epitech.eu>
2025-12-01 09:39:41 +0100

    fix(ecs): change way to set inputPLayer

--- GameEngine/Systems/InputSystem.cpp
+++ GameEngine/Systems/InputSystem.cpp
@@ -11,10 +11,9 @@ void InputSystem::update(EcsManager & ecs) {
     for (auto& entity : ecs.getEntities()) {
         auto inputPtr = entity->getComponent<InputPlayer>();
         if (!inputPtr) continue;
-
-        inputPtr->_up = sf::Keyboard::isKeyPressed(sf::Keyboard::W);
-        inputPtr->_down = sf::Keyboard::isKeyPressed(sf::Keyboard::S);
-        inputPtr->_left = sf::Keyboard::isKeyPressed(sf::Keyboard::A);
-        inputPtr->_right = sf::Keyboard::isKeyPressed(sf::Keyboard::D);
+        inputPtr->setDown(sf::Keyboard::isKeyPressed(sf::Keyboard::S));
+        inputPtr->setUp(sf::Keyboard::isKeyPressed(sf::Keyboard::W));
+        inputPtr->setLeft(sf::Keyboard::isKeyPressed(sf::Keyboard::A));
+        inputPtr->setRight(sf::Keyboard::isKeyPressed(sf::Keyboard::D));
     }
 }
